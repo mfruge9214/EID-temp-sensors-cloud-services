c8848d3640e41e2c684524d30757a78e8368a4b6
Mike Fruge <mifr6526@colorado.edu>
2021-03-02 22:07:34 -0700

    Added Super Project Proposal

--- displayScript.js
+++
@@ -1,9 +0,0 @@
-      // Send websocket message function
-  $("#sensor1_t_req").click(function(evt) {
-      $("#sensor1_out_data").html("Clicked");
-      // log("Sending Message: "+$("#message").val());
-      // ws.send($("#message").val());
-  });
-
-
-  
